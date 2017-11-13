1e4d545146ad8f61cd33fadd8980c067a2b54298
Denis <denisdenisdenis@bigmir.net>
2017-11-14 01:51:40 +0200

    create company db

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.12.4"
 libraryDependencies ++= List(
   "com.typesafe.slick" %% "slick" % "3.2.1",
   "org.postgresql" % "postgresql" % "42.1.4",
+  "org.slf4j" % "slf4j-nop" % "1.7.10"
 )
         
--- src/main/scala/AirportApp.scala
+++ src/main/scala/AirportApp.scala
@@ -1,9 +1,22 @@
 import slick.jdbc.PostgresProfile.api._
+
 import scala.concurrent.Await
 import model._
+
+import scala.concurrent.duration.Duration
 
 
 object AirportApp extends App {
   println("Hello airport")
+
+  def exec[T](program: DBIO[T]): T = Await.result(db.run(program), Duration.Inf)
   val db = Database.forURL("jdbc:postgresql://127.0.0.1/airport?user=postgres&password=123456789")
+
+  val companyRepository = new CompanyRepository(db)
+  val passengerRepository = new PassengerRepository(db)
+  val tripRepository = new TripRepository(db)
+
+  exec(CompanyTable.table.schema.create)
+  println(CompanyTable.table.schema.createStatements.mkString)
+
 }
