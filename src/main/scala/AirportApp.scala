040286fde9d616578105f919c15922607d81c074
DenisLobur <denisdenisdenis@bigmir.net>
2017-11-15 22:53:33 +0200

    #72

--- src/main/scala/AirportApp.scala
+++ src/main/scala/AirportApp.scala
@@ -1,10 +1,7 @@
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.Await
-import model.{Passenger, _}
+import model._
 import data._
 
 import scala.concurrent.duration.Duration
@@ -45,7 +42,9 @@ object AirportApp extends App {
     * Find the names of different passengers that ever travelled more than once occupying seats with the same number
     */
   def _63(): Unit = {
-    val query = passInTripRepository.table.join(passengerRepository.table).on(_.idPsg === _.idPsg)
+    val query = passInTripRepository.table
+      .join(passengerRepository.table)
+      .on(_.idPsg === _.idPsg)
       .groupBy {
         case (pasintrip, pas) => (pasintrip.place, pasintrip.idPsg, pas.name)
       }
@@ -120,17 +119,63 @@ object AirportApp extends App {
         case (_, group) => group.max
       }
 
-    val query = subQuery.filter {
-      _ in max
-    }
-    .length
-    .result
+    val query = subQuery
+      .filter {
+        _ in max
+      }
+      .length
+      .result
 
     println("\n=> " + query.statements.mkString.toUpperCase)
     println(exec(query) / 2)
   }
 
+  /** Task #72
+    * Among the customers using a single airline, find distinct passengers who have flown most frequently.
+    * Result set: passenger name, number of trips. */
+  def _72(): Unit = {
+    val passAndComp = passInTripRepository.table
+      .join(tripRepository.table)
+      .on(_.tripNo === _.tripNo)
+      .groupBy {
+        case (pasintrip, pas) => (pasintrip.idPsg, pas.idComp)
+      }
+      .map {
+        case ((idPas, idComp), group) => (idPas, idComp, group.size)
+      }
+
+    val companyPassMaximum = passAndComp
+      .map {
+        case (idPas, idComp, maximum) => maximum
+      }
+      .groupBy {
+        _ => true
+      }
+      .map {
+        case (_, group) => group.max
+      }
+
+    val idPasWithMax = passAndComp
+      .filter {
+        case pas => pas._3 in companyPassMaximum
+      }
+      .map {
+        case pas => (pas._1, pas._3)
+      }
+
+    val passNames = idPasWithMax
+      .join(passengerRepository.table)
+      .on(_._1 === _.idPsg)
+      .map {
+        case (idPasMax, pas) => (pas.name, idPasMax._2)
+      }
+
+    println("\n=> " + passNames.result.statements.mkString.toUpperCase)
+    exec(passNames.result).foreach(println)
+  }
+
   _63()
   _67()
   _68()
+  _72()
 }
